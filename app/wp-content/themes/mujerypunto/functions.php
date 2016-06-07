2e333168dbdcf2e044ec9ee594409c3c1d88f89e
Marlene Jamett <marlenejamett@marlenejamett.local>
2016-06-07 18:29:33 -0300

    update css y cambios

--- app/wp-content/themes/mujerypunto/functions.php
+++ app/wp-content/themes/mujerypunto/functions.php
@@ -543,4 +543,5 @@ add_filter('post_gallery', 'custom_gallery', 11, 2);
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
 
+
 ?>
--- app/wp-content/themes/mujerypunto/index.php
+++ app/wp-content/themes/mujerypunto/index.php
@@ -14,6 +14,14 @@
 			        <div class="swiper-pagination"></div>
 			    </div>
 
+			    <div class="secction-reciente">
+			    	<div class="title-bg"><div class="line-bg"></div><h2 class="title-section">Lo más reciente</h2></div>
+			    	<div class="content-reciente">
+
+			    		<?php get_template_part('loop-reciente'); ?>  
+			    	</div>
+			    </div>
+
 			    <div class="body-sidebar">
 
 			    	<div class="section-loop">
@@ -68,6 +76,108 @@
 			    </div>
 			    <div class="sidebar">
 			    	<?php get_sidebar(); ?>
+			    </div>
+
+			    <div class="secction-videos">
+			    	<div class="title-bg"><div class="line-bg"></div><h2 class="title-section">Videos</h2></div>
+		    		<div class="post-video-principal">
+						<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>> 
+							<?php 
+								global $post, $posts;
+								$output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
+								$first_img = $matches [1] [0];
+							?>
+							<div class="imagen-contentpost">
+								<a href="<?php the_permalink(); ?>">
+								<div class="imagen-post" style="background-image:url('<?php global $post; $thumbID = get_post_thumbnail_id( $post->ID ); if($thumbID){$imgDestacada = wp_get_attachment_url( $thumbID ); echo $imgDestacada; }else{ echo $first_img;}?>')">
+									<div class="bg-contentpost"></div>
+								</div>	
+								</a>
+							</div>
+							<div class="content-post">
+								<div class="post-inside">
+									<div class="video-info">
+										<div class="vistas"><i class="fa fa-eye"></i> Vistas</div>
+									</div>
+									<div class="social-share">
+										<ul>
+											<li><a href="javascript:fbShare('<?php echo the_permalink(); ?>', '<?php the_title(); ?>', '<?php the_title(); ?>', '<?php echo the_permalink(); ?>', 520, 350)"><i class="fa fa-facebook"></i></a></li>
+											<li><a href="javascript:twShare('<?php echo the_permalink(); ?>', '<?php the_title(); ?> - vía: @revistacosas', 520, 350)"><i class="fa fa-twitter"></i></a></li>
+											<li><a target="_blank" href="https://plus.google.com/share?url=<?php the_permalink(); ?>" onclick="window.open('https://plus.google.com/share?url=<?php the_permalink(); ?>','gplusshare','width=600,height=400,left='+(screen.availWidth/2-225)+',top='+(screen.availHeight/2-150)+'');return false;"><i class="fa fa-google-plus"></i></a></li>
+											<li class="whatsapp"><a href="whatsapp://send?text=<?php the_title(); ?> – <?php urlencode(the_permalink()); ?>" data-action="share/whatsapp/share"><i class="fa fa-whatsapp"></i></a></li>
+										</ul>
+									</div>
+									<a href="<?php the_permalink(); ?>"><h2 class="title-post"><?php the_title(); ?></h2></a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="post-list">
+						<div class="post-video">
+							<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>> 
+								<?php 
+									global $post, $posts;
+									$output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
+									$first_img = $matches [1] [0];
+								?>
+								<div class="imagen-contentpost">
+									<a href="<?php the_permalink(); ?>">
+									<div class="imagen-post" style="background-image:url('<?php global $post; $thumbID = get_post_thumbnail_id( $post->ID ); if($thumbID){$imgDestacada = wp_get_attachment_url( $thumbID ); echo $imgDestacada; }else{ echo $first_img;}?>')">
+										<div class="bg-contentpost"></div>
+									</div>	
+									</a>
+								</div>
+								<div class="content-post">
+									<div class="post-inside">
+										<div class="video-info">
+											<div class="vistas"><i class="fa fa-eye"></i> Vistas</div>
+										</div>
+										<div class="social-share">
+											<ul>
+												<li><a href="javascript:fbShare('<?php echo the_permalink(); ?>', '<?php the_title(); ?>', '<?php the_title(); ?>', '<?php echo the_permalink(); ?>', 520, 350)"><i class="fa fa-facebook"></i></a></li>
+												<li><a href="javascript:twShare('<?php echo the_permalink(); ?>', '<?php the_title(); ?> - vía: @revistacosas', 520, 350)"><i class="fa fa-twitter"></i></a></li>
+												<li><a target="_blank" href="https://plus.google.com/share?url=<?php the_permalink(); ?>" onclick="window.open('https://plus.google.com/share?url=<?php the_permalink(); ?>','gplusshare','width=600,height=400,left='+(screen.availWidth/2-225)+',top='+(screen.availHeight/2-150)+'');return false;"><i class="fa fa-google-plus"></i></a></li>
+												<li class="whatsapp"><a href="whatsapp://send?text=<?php the_title(); ?> – <?php urlencode(the_permalink()); ?>" data-action="share/whatsapp/share"><i class="fa fa-whatsapp"></i></a></li>
+											</ul>
+										</div>
+										<a href="<?php the_permalink(); ?>"><h2 class="title-post"><?php the_title(); ?></h2></a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="post-video">
+							<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>> 
+								<?php 
+									global $post, $posts;
+									$output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
+									$first_img = $matches [1] [0];
+								?>
+								<div class="imagen-contentpost">
+									<a href="<?php the_permalink(); ?>">
+									<div class="imagen-post" style="background-image:url('<?php global $post; $thumbID = get_post_thumbnail_id( $post->ID ); if($thumbID){$imgDestacada = wp_get_attachment_url( $thumbID ); echo $imgDestacada; }else{ echo $first_img;}?>')">
+										<div class="bg-contentpost"></div>
+									</div>	
+									</a>
+								</div>
+								<div class="content-post">
+									<div class="post-inside">
+										<div class="video-info">
+											<div class="vistas"><i class="fa fa-eye"></i> Vistas</div>
+										</div>
+										<div class="social-share">
+											<ul>
+												<li><a href="javascript:fbShare('<?php echo the_permalink(); ?>', '<?php the_title(); ?>', '<?php the_title(); ?>', '<?php echo the_permalink(); ?>', 520, 350)"><i class="fa fa-facebook"></i></a></li>
+												<li><a href="javascript:twShare('<?php echo the_permalink(); ?>', '<?php the_title(); ?> - vía: @revistacosas', 520, 350)"><i class="fa fa-twitter"></i></a></li>
+												<li><a target="_blank" href="https://plus.google.com/share?url=<?php the_permalink(); ?>" onclick="window.open('https://plus.google.com/share?url=<?php the_permalink(); ?>','gplusshare','width=600,height=400,left='+(screen.availWidth/2-225)+',top='+(screen.availHeight/2-150)+'');return false;"><i class="fa fa-google-plus"></i></a></li>
+												<li class="whatsapp"><a href="whatsapp://send?text=<?php the_title(); ?> – <?php urlencode(the_permalink()); ?>" data-action="share/whatsapp/share"><i class="fa fa-whatsapp"></i></a></li>
+											</ul>
+										</div>
+										<a href="<?php the_permalink(); ?>"><h2 class="title-post"><?php the_title(); ?></h2></a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
 			    </div>
 
 		</section>
